8c25d29f9c2eedad39be4f25a4bbd9714242c7a2
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-08-19 19:11:37 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	
+	
+	public Department() {
+		
+	}
+	
+	
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+	
+	@Override
+	public String toString() {
+	
+		return "Id : " + id + ", nome: " + name;
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+
+}
