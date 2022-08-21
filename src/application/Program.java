a1b6fb981b13f8f756dba53d0ec74a34b51a6975
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-08-21 17:23:29 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -35,9 +34,14 @@ public class Program {
 		}
 		
 		System.out.println("=== TEST 4: SELLER insert===");
-		sellerDao.insert(new Seller(null,"Rafael", "rafael@gmail", new java.util.Date(), 4000.00, new Department(3, null)));
+		//sellerDao.insert(new Seller(null,"Rafael", "rafael@gmail", new java.util.Date(), 4000.00, new Department(3, null)));
 		
+		System.out.println("=== TEST 5: SELLER update===");
+		Seller seller = sellerDao.findById(2) ;
+		seller.setName("Fred");
+		sellerDao.update(seller);
 		
+		sellerDao.update(new Seller(17,"Bruce","bruce@gmail", new java.util.Date(), 10000.00, new Department(1, null)));
 	}
 
 }
